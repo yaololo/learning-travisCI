1c999b2283fd37359f5375e9ce71d6fba2936e02
zhuangyongming <yongming.zhuang@moneysmart.com>
2019-01-21 16:23:31 +0800

    Create index test file

---
+++ index.test.js
@@ -0,0 +1,21 @@
+const hello = require('./hello');
+const handleListen = require('./listeningHandler');
+
+it('should call res.send() with Hello World!', () => {
+  const send = jest.fn();
+  const res = {
+    send,
+  };
+  hello({}, res);
+  expect(send.mock.calls).toHaveLength(1);
+  expect(send.mock.calls[0][0]).toBe('Hello World!');
+});
+
+
+it('should call log with Example app...', () => {
+  const PORT = 3000;
+  const log = jest.fn();
+  handleListen(log, PORT);
+  expect(log.mock.calls).toHaveLength(1);
+  expect(log.mock.calls[0][0]).toBe(`Example app listening on port ${PORT.toString()}!`);
+});
